c1ba66f6bf1c529b5395d71e97ec13e65403ca3a
elMat3r <apolo.leyton@zonavirtual.uisek.cl>
2025-05-26 10:07:37 -0400

    Avance en clases: 26-05
    Intentar de aplicar el Overlap en la nave, pero me tira un error de seteo con el AddForce()

--- Proyecto_Eva2_Ciencias3_ApoloLeyton/Assets/Scripts/Player_Controls.cs
+++ Proyecto_Eva2_Ciencias3_ApoloLeyton/Assets/Scripts/Player_Controls.cs
@@ -5,8 +5,11 @@ public class Player_Controls : MonoBehaviour
     [SerializeField] private float moveSpd;
     [SerializeField] private float rotationSpd;
     [SerializeField] private float radio;
+    [SerializeField] private float overlapRadio;
     [SerializeField] private float maxDistance;
     [SerializeField] private float atractForce;
+    [SerializeField] private float pushForce;
+    [SerializeField] private Transform playerPosition;
 
     public float x, y;
     public LayerMask layerMask;
@@ -19,6 +22,10 @@ public class Player_Controls : MonoBehaviour
         transform.Rotate(0, 0, y * Time.deltaTime * rotationSpd);
         transform.Translate(x * Time.deltaTime * moveSpd, 0, 0);
 
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            RepulsionSystem(playerPosition.position, radio);
+        }
         VacuumSystem();
     }
     public void VacuumSystem()
@@ -38,6 +45,17 @@ public class Player_Controls : MonoBehaviour
                     rb.AddForce(-dir.normalized * atractForce, ForceMode.Force);
                 }
             }
+        }
+    }
+    public void RepulsionSystem(Vector3 center, float ratio)
+    {
+        Collider[] hitsColliders = Physics.OverlapSphere(center, ratio);
+        for (int i = 0; i < hitsColliders.Length; i++)
+        {
+            Rigidbody rb = hitsColliders[i].attachedRigidbody;
+            //hitsColliders[i].transform.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f);
+            Vector3 dir = hitsColliders[i].transform.position - playerPosition.position;
+            rb.AddForce(dir.normalized * 1 / dir.magnitude * pushForce, ForceMode.Impulse);
         }
     }
     private void OnDrawGizmos()

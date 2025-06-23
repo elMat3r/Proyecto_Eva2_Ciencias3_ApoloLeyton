205e0cf707fc270e1a8f990d60f148846880e4e1
elMat3r <apolo.leyton@zonavirtual.uisek.cl>
2025-06-23 09:38:43 -0400

    Avances en clases: 23-06

--- Proyecto_Eva2_Ciencias3_ApoloLeyton/Assets/Scripts/BlackHole_Scripts/BlackHole_Function.cs
+++ Proyecto_Eva2_Ciencias3_ApoloLeyton/Assets/Scripts/BlackHole_Scripts/BlackHole_Function.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class BlackHole_Function : MonoBehaviour
 {
-    [SerializeField] private float upthrust;
+    //[SerializeField] private float upthrust;
     [SerializeField] private float viscocity;
     [SerializeField] private Rigidbody rb;
     public void BlackHolePhysics()
     {
-        rb.AddForce(Vector3.up * upthrust);
-
-        rb.AddForce(rb.linearVelocity * viscocity);
+        //rb.AddForce(Vector3.positiveInfinity * upthrust);
+        rb.AddForce(-rb.linearVelocity * viscocity);
     }
 }
--- Proyecto_Eva2_Ciencias3_ApoloLeyton/Assets/Scripts/BlackHole_Scripts/BlackHole_Zone.cs
+++ Proyecto_Eva2_Ciencias3_ApoloLeyton/Assets/Scripts/BlackHole_Scripts/BlackHole_Zone.cs
@@ -5,7 +5,7 @@ public class BlackHole_Zone : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         BlackHole_Function blackHole_Function = other.GetComponent<BlackHole_Function>();
-        if(blackHole_Function != null)
+        if (blackHole_Function != null)
         {
             blackHole_Function.BlackHolePhysics();
         }
